3abf12d748acad26661619def61dc9251c6b6b16
zevilz <zacharygriffee@gmail.com>
2024-12-19 11:40:00 -0500

    rxprotoplex bump

--- tests/test.js
+++ tests/test.js
@@ -1,8 +1,10 @@
-import {test} from "brittle";
+import {test, solo} from "brittle";
 import {WebSocketServer} from "ws";
 import {fromWebSocket} from "../lib/fromWebSocket.js";
 import {getWebSocketURL} from "./getWebSocketUrl.js";
-import {listenAndConnectionAndRead$, connectAndSend, withEncoding} from "rxprotoplex";
+import {listenAndConnectionAndRead$, connectAndSend, withEncoding, destroy} from "rxprotoplex";
+import b4a from "b4a";
+import {take} from "rxjs";
 
 async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
@@ -46,13 +48,39 @@ test("basic websocket plex test", async t => {
     });
 });
 
+test("Large data", async t => {
+    t.plan(1);
+    const largeMessage = b4a.from("x".repeat(1024 * 1024)); // 1 MB message
+    let serverPlex;
+    const [server, url] = await createServer((conn) => {
+        serverPlex = conn;
+    });
+    const received = [];
+    const plex = fromWebSocket(url);
+    await sleep(10);
+    const send = connectAndSend(plex);
+    listenAndConnectionAndRead$(serverPlex).pipe(take(1)).subscribe(({data}) => {
+        t.alike(data, largeMessage);
+    });
+
+    send(largeMessage)
+    t.teardown(() => {
+        destroy(plex);
+        destroy(serverPlex);
+        server.close();
+    })
+});
+
 test("error on websocket propagates", async t => {
     t.plan(1);
+
     const plex = fromWebSocket("wss://localhost:12345");
     const sub = plex.close$.subscribe({error: e => {
         t.is(e.code, "ECONNREFUSED")
     }});
-    t.teardown(() => sub.unsubscribe());
+    t.teardown(() => {
+        sub.unsubscribe();
+    });
 });
 
 
